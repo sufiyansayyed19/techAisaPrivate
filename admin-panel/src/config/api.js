c569b712812800178a05e5e42ad9c9e62ffe62f9
Sufiyan Sayyed <108610733+sufiyansayyed19@users.noreply.github.com>
2025-07-12 01:16:29 +0530

    Update api.js

--- admin-panel/src/config/api.js
+++ admin-panel/src/config/api.js
@@ -1,3 +1,3 @@
 // config spcify to which url we are making call 
-export const API_BASE_URL = 'https://techasiapivate-backend.onrender.com/api';
+export const API_BASE_URL = 'https://techaisaprivate.onrender.com/api';
 
